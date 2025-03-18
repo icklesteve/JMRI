770fe242fee0a53d87b52d59d10147cf79ce1468
Steve Young <icklesteve@yahoo.com>
2025-03-18 12:41:24 +0000

    Update ConditionalListEdit.java
    unused import

--- java/src/jmri/jmrit/conditional/ConditionalListEdit.java
+++ java/src/jmri/jmrit/conditional/ConditionalListEdit.java
@@ -4,7 +4,6 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
