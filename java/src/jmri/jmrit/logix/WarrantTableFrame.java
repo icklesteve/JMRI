d98e4d980c382b49ebca61976dac7f3161ebad4f
Steve Young <icklesteve@yahoo.com>
2025-05-23 12:21:48 +0100

    Revert "Update WarrantTableFrame.java"
    This reverts commit d301e6077832d08f5519ff84c5fcdff1cce6606b.

--- java/src/jmri/jmrit/logix/WarrantTableFrame.java
+++ java/src/jmri/jmrit/logix/WarrantTableFrame.java
@@ -26,7 +26,6 @@ import javax.swing.table.TableRowSorter;
 
 import jmri.InstanceManager;
 import jmri.Path;
-import jmri.util.ThreadingUtil;
 import jmri.util.swing.JmriJOptionPane;
 import jmri.util.swing.JmriMouseEvent;
 import jmri.util.swing.JmriMouseListener;
@@ -247,7 +246,7 @@ public class WarrantTableFrame extends jmri.util.JmriJFrame implements JmriMouse
         setJMenuBar(menuBar);
         addHelpMenu("package.jmri.jmrit.logix.WarrantTable", true);
 
-        ThreadingUtil.runOnGUI( () -> pack() );
+        pack();
     }
 
     private void haltAllAction() {
